542837673beab9509ed02eaac8f7342a69383c41
MCOLLIGAN-LT\mcolligan <mcolligan@drintl.com>
2020-05-26 11:32:46 -0400

    custom query select all checkbox onclick functions

--- src/Components/Reporting/Reporting.jsx
+++ src/Components/Reporting/Reporting.jsx
@@ -7,6 +7,9 @@ import SecondarySideNav from './../../Shared/SecondarySideNav/SecondarySideNav';
 
 import './reporting.scss';
 
+//for populating query filter menu - would actually be pulled from database
+let states = ['Alabama', 'Alaska', 'Arizona', 'Arkansas', 'California', 'Colorado', 'Connecticut', 'Delaware', 'District of Columbia', 'Florida', 'Georgia', 'Hawaii', 'Idaho', 'Illinois', 'Indiana', 'Iowa', 'Kansas', 'Kentucky', 'Louisiana', 'Maine', 'Maryland', 'Massachusetts', 'Michigan', 'Minnesota', 'Mississippi', 'Missouri', 'Montana', 'Nebraska', 'Nevada', 'New Hampshire', 'New Jersey', 'New Mexico', 'New York', 'North Carolina', 'North Dakota', 'Ohio', 'Oklahoma', 'Oregon', 'Pennsylvania', 'Puerto Rico', 'Rhode Island', 'South Carolina', 'South Dakota', 'Tennessee', 'Texas', 'Utah', 'Vermont', 'Virginia', 'Washington', 'West Virginia', 'Wisconsin', 'Wyoming'];
+
 class Reporting extends Component {
     constructor(props) {
         super(props);
@@ -14,17 +17,23 @@ class Reporting extends Component {
             sideNavActiveIndexes: [0, 1, 2],
             activeItemMain: 'Data Exports',
             queryFilterMenuData: [],
-            queryFilterCriteria: {timePeriod: [], location: [], segment: []}
+            queryFilterCriteria: {}
         }
     }
 
     componentDidMount = () => {
         window.scrollTo(0, 0);
 
-        //will load from view or api call
+        //temp function for states list - would load from database
+        let statesList = [];
+        for (let i = 0; i < states.length; i++) {
+            statesList.push({value: states[i], category: 'location'});
+        }
+        //will load from api call
         let filterData = [
             {
                 title: 'Time Period',
+                category: 'timePeriod',
                 content: [
                     {value: 2020, category: 'timePeriod'},
                     {value: 2019, category: 'timePeriod'},
@@ -32,22 +41,19 @@ class Reporting extends Component {
                     {value: 2017, category: 'timePeriod'},
                     {value: 2016, category: 'timePeriod'},
                     {value: 2015, category: 'timePeriod'}
-                ]
+                ],
+                showAll: true
             },
             {
                 //cutting list short for this site template
                 title: 'Location',
-                content: [
-                    {value: 'Alaska', category: 'location'},
-                    {value: 'Alabama', category: 'location'},
-                    {value: 'Arkansas', category: 'location'},
-                    {value: 'Arizona', category: 'location'},
-                    {value: 'California', category: 'location'},
-                    {value: 'Colorado', category: 'location'}
-                ]
+                category: 'location',
+                content: statesList,
+                showAll: false
             },
             {
                 title: 'Customer Segment',
+                category: 'segment',
                 content: [
                     {value: 'Commercial', category: 'segment'},
                     {value: 'Industrial', category: 'segment'},
@@ -55,11 +61,27 @@ class Reporting extends Component {
                     {value: 'Residential', category: 'segment'},
                     {value: 'Utility', category: 'segment'},
                     {value: 'Unclassified', category: 'segment'}
-                ]
+                ],
+                showAll: true
             }
         ];
 
-        this.setState({ queryFilterMenuData: filterData })
+        let filterCriteria = {
+            timePeriod: {
+                allSelected: false,
+                selected: []
+            }, 
+            location: {
+                allSelected: false,
+                selected: []
+            }, 
+            segment: {
+                allSelected: false,
+                selected: []
+            }
+        };
+
+        this.setState({ queryFilterMenuData: filterData, queryFilterCriteria: filterCriteria })
     }
 
     handleItemClickMain = (e, { name }) => {
@@ -67,18 +89,47 @@ class Reporting extends Component {
     }
 
     handleSecondaryItemClick = (e, data) => {
+        
         const {queryFilterCriteria} = this.state;
+        let category = queryFilterCriteria[data.category].selected,
+            allSelected = queryFilterCriteria[data.category].allSelected,
+            value = data.value;
+            
+        if (allSelected) {
+            queryFilterCriteria[data.category].allSelected = false;
+        }
 
         if (data) {
-            if (queryFilterCriteria[data.category].includes(data.value)) {
-                queryFilterCriteria[data.category].pop(data.value);
+            if (category.includes(value)) {
+                category.splice(category.indexOf(value), 1);
             } else {
-                queryFilterCriteria[data.category].push(data.value);
+                category.push(value);
             }
     
             this.setState({ queryFilterCriteria });
-            console.log(this.state.queryFilterCriteria);
         }
+    }
+
+    handleSecondaryItemSelectAll = (e, info, data) => {
+        const {queryFilterCriteria, queryFilterMenuData} = this.state;
+        
+        if (info.checked) {
+            let targetMenu = queryFilterMenuData.find((obj => obj.category === data.category)).content,
+                targetCriteria = queryFilterCriteria[data.category].selected;
+                
+            for (let i = 0; i < targetMenu.length; i++) {
+                let value = targetMenu[i].value;
+                if (!targetCriteria.includes(value)) {
+                    targetCriteria.push(value);
+                }
+            }
+            queryFilterCriteria[data.category].allSelected = true;
+
+        } else {
+            queryFilterCriteria[data.category].selected = [];
+            queryFilterCriteria[data.category].allSelected = false;
+        }
+        this.setState({ queryFilterCriteria });
     }
 
     handleSideNavMenuClick = (e, titleProps) => {
@@ -97,7 +148,7 @@ class Reporting extends Component {
     }
 
   render() {
-    const {activeItemMain, sideNavActiveIndexes, queryFilterMenuData} = this.state;
+    const {activeItemMain, sideNavActiveIndexes, queryFilterMenuData, queryFilterCriteria} = this.state;
 
     const mainSideNavInfo = [
         {name: 'Data Exports', iconName: 'file alternate'},
@@ -106,19 +157,19 @@ class Reporting extends Component {
 
     //placeholder
     let dropdownYears = [
-        {key: 1, value: '2015', value: 2015},
-        {key: 2, value: '2016', value: 2016},
-        {key: 3, value: '2017', value: 2017},
-        {key: 4, value: '2018', value: 2018},
-        {key: 5, value: '2019', value: 2019},
-        {key: 6, value: '2010', value: 2020},
+        {key: 1, value: 2015, text: 2015},
+        {key: 2, value: 2016, text: 2016},
+        {key: 3, value: 2017, text: 2017},
+        {key: 4, value: 2018, text: 2018},
+        {key: 5, value: 2019, text: 2019},
+        {key: 6, value: 2020, text: 2020}
     ];
 
     //placeholder
     let dropdownOrgs = [
-        {key: 1, value: 'Org A', value: 1},
-        {key: 2, value: 'Org B', value: 2},
-        {key: 3, value: 'Org C', value: 3},
+        {key: 1, value: 'Org A', text: 'Org A'},
+        {key: 2, value: 'Org B', text: 'Org B'},
+        {key: 3, value: 'Org C', text: 'Org C'}
     ];
 
     const secondSideNavContent = (
@@ -133,10 +184,13 @@ class Reporting extends Component {
                             </Accordion.Title>
                             <Accordion.Content active={sideNavActiveIndexes.includes(i)}>
                                 <List relaxed>
-                                    {data.content.map((data, i) => {
+                                    <List.Item key={i}>
+                                        <Checkbox label='Select All' checked={queryFilterCriteria[data.category].allSelected} onClick={(e, info) => this.handleSecondaryItemSelectAll(e, info, data)} />
+                                    </List.Item>
+                                    {data.content.map((contentData, i) => {
                                         return (
-                                            <List.Item key={i} onClick={(e) => this.handleSecondaryItemClick(e, data)}>
-                                                <Checkbox label={data.value} />
+                                            <List.Item key={i}>
+                                                <Checkbox label={contentData.value} checked={queryFilterCriteria[data.category].allSelected || queryFilterCriteria[data.category].selected.includes(contentData.value)} onClick={(e) => this.handleSecondaryItemClick(e, contentData)} />
                                             </List.Item>
                                         )
                                     })}
